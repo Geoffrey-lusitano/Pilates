baa475ae2fd8eb7140f980819f3a97a73d34c8a2
geoffrey lusitano <g.bl@outlook.fr>
2022-07-25 11:24:44 +0200

    typetext animation header

--- src/components/App__header.vue
+++ src/components/App__header.vue
@@ -3,8 +3,8 @@
         <div class="logo">
             <img src="../assets/logo.png" alt="logo" />
         </div>
-        <h1>Les valeurs :
-            <span class="typed__text"></span>
+        <h1>
+            <span class="typed__text"> {{ typeValue }}</span>
         </h1>
 
 
@@ -14,9 +14,55 @@
 <script>
 export default {
   name: 'App__header',
-  props: {
-    msg: String
+  data: () => {
+    return {
+        typeValue: '',
+        typeStatus: false,
+        typeArray: ['Concentration', 'Centrage', 'Respiration', 'Isolation', 'précision', 'Fluidité'],
+        typingSpeed: 50,
+        erasingSpeed: 50,
+        newTextDelay: 2000,
+        typeArrayIndex: 0,
+        charIndex: 0,
+    }
+  },
+  methods: {
+    typeText() {
+        if(this.charIndex < this.typeArray[this.typeArrayIndex].length) {
+            if(!this.typeStatus)
+                this.typeStatus = true;
+
+            this.typeValue += this.typeArray[this.typeArrayIndex].charAt(this.charIndex);
+            this.charIndex += 1;
+
+            setTimeout(this.typeText, this.typingSpeed);
+        } else {
+            this.typeStatus = false;
+            setTimeout(this.eraseText, this.newTextDelay);
+        }
+    },
+    eraseText() {
+        if(this.charIndex > 0) {
+            if(!this.typeStatus)
+                this.typeStatus = true;
+            this.typeValue = this.typeArray[this.typeArrayIndex].substring(0, this.charIndex - 1);
+            this.charIndex -= 1;
+
+            setTimeout(this.eraseText, this.erasingSpeed);
+        } else {
+            this.typeStatus = false;
+            this.typeArrayIndex += 1;
+            if(this.typeArrayIndex >= this.typeArray.length)
+                this.typeArrayIndex = 0;
+            
+            setTimeout(this.typeText, this.typingSpeed + 1000);
+        }
+    },
+  },
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
   }
+
 }
 </script>
 
